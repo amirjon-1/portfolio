0992cbeda82e74cafc1e693d128b84370c56555e
amirjon-1 <amirjon5321@gmail.com>
2024-12-25 16:07:35 -0500

    added another project

--- src/constants/index.js
+++ src/constants/index.js
@@ -46,6 +46,38 @@ export const navLinks = [
         },
       ],
     },{
+      title: 'Consensus Corner',
+      desc: 'Consensus Corner is a platform that allows users to share their views and opinions on various topics regarding politics. It provides a space for users to express their thoughts and opinions in a safe and respectful environment. Most of all it is secure.      ',
+      subdesc:
+      'The website is designed to provide a secure way of sharing information. It uses basic html, css and js to deliver a responsive design thats both simple and functional. When initially signing up, it uses a third party website for the user to generate a public and private key (created using the same email and passphrase). This public key and encrypted private key can be downloaded from that third party website and can be uploaded when signing up and posting. The public key is used for making an account to make sure the person signing up can be verified. When creating a post the user must enter their encrypted private key and passphrase to decrypt it in order to post a message (it wont work otherwise). It displays this post and also stores it in Firebase. The message, along with the signed version of the message (uses openpgp to sign), and the public key is stored in firebase. When a post is displayed it allows anyone to check the signed message and the authors public key (under details). This can be used to verify if the message was written by the author (this can also be done through a third party website). The public key can be seen on a public directory (under construction) that allows anyone to connect a public key to an identity (uses a form of higher verification). This guarantees that identity fraud or something similar will be extremely rare. People can also upvote and downvote the policy. They can also add comments. ',
+      href: 'https://amirjon-1.github.io/ccalproject/',
+      texture: '/assets/s.mp4',
+      logo: '/assets/consenselogo.gif',
+      logoStyle: {
+        backgroundColor: '#FFFFFF',
+        border: '0.2px solid rgba(208, 213, 221, 1)',
+        boxShadow: '0px 0px 60px 0px rgba(35, 131, 96, 0.3)',
+      },
+      spotlight: '/assets/spotlight3.png',
+      tags: [
+        {
+          id: 1,
+          name: 'HTML',
+          path: '/assets/html.webp',
+        },{
+          id: 2,
+          name: 'CSS',
+          path: '/assets/css.webp',
+        },{
+          id: 3,
+          name: 'JS',
+          path: '/assets/js.png',
+        },
+      ],
+    },
+    
+    
+    {
       title: 'Vacatscape - Vacation Rental Homes',
       desc: 'Vacatscape is a platform for booking short-term vacation rentals, featuring a range of unique accommodations. It allows travelers to easily find and reserve properties that suit their preferences and budgets.      ',
       subdesc:
